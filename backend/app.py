d3694141251c7b29d2470c48bb54921f38e07dd3
mahammadsaif <saifshaik1305@gmail.com>
2025-06-25 17:59:16 +0530

    Updated app.py for Render deployment and dataset parsing

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 from rapidfuzz import fuzz
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -86,4 +88,5 @@ def check_slang():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
